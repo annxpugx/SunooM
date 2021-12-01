27c27e2b55d4620253d1c407104cf0449788388e
MIOJOLU <64047127+MIOJOLU@users.noreply.github.com>
2021-12-01 10:04:23 -0300

    Aumentando velocidade do ataque

--- Assets/Scripts/PlayerFightBehaviour.cs
+++ Assets/Scripts/PlayerFightBehaviour.cs
@@ -56,22 +56,22 @@ public class PlayerFightBehaviour : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D) || dpad.xAxis.value == 1)
         {
-            transform.Translate(0.05f, 0f, 0f);
+            transform.Translate(0.1f, 0f, 0f);
         }
 
         if (Input.GetKey(KeyCode.A) || dpad.xAxis.value == -1)
         {
-            transform.Translate(-0.05f, 0f, 0f);
+            transform.Translate(-0.1f, 0f, 0f);
         }
 
         if (Input.GetKey(KeyCode.S) || dpad.yAxis.value == -1)
         {
-            transform.Translate(0f, 0f, -0.05f);
+            transform.Translate(0f, 0f, -0.1f);
         }
 
         if (Input.GetKey(KeyCode.W) || dpad.yAxis.value == 1)
         {
-            transform.Translate(0f, 0f, 0.05f);
+            transform.Translate(0f, 0f, 0.1f);
         }
 
         if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && dpad.yAxis.value == 0 && dpad.xAxis.value == 0)
